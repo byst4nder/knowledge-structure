8fab2c0ee0aad089addac1891781c93d2c0b85f8
“yourVincent.Wu <vincent.wu@goertek.com>
2019-03-10 00:20:40 +0800

    QGridLayout中addWidget的参数设置

---
+++ python编程/python界面PyQT5/PyQt5/003PyQt-Layout Management03-Review.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+
+In this example, we create a more
+complicated window layout using
+the QGridLayout manager.
+
+"""
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
+    QTextEdit, QGridLayout, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)  # 设置的是title这几个Label与框之间的距离。
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)   # 这个数字是，grid中，定标用的，第三行，第一列，
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+6个参数表示控件名，行，列，占用行数，占用列数，对齐方式
