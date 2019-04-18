e843400346a6c5b44b50280a8342d657975e67af
“yourVincent.Wu <vincent.wu@goertek.com>
2019-04-18 18:24:51 +0800

    BoxLayout更新

--- python编程/python界面PyQT5/PyQt5/003PyQt-Layout Management01-BoxLayout.py
+++ python编程/python界面PyQT5/PyQt5/003PyQt-Layout Management01-BoxLayout.py
@@ -30,13 +30,16 @@ class Example(QWidget):
         cancelButton = QPushButton("Cancel")
 
         hbox = QHBoxLayout()
-        hbox.addStretch(1)
+        hbox.addStretch(1)   # 平均分配Layout通过addStretch控制空间距离 分割比例。来实现定位。
         hbox.addWidget(okButton)
+        hbox.addStretch(1)
         hbox.addWidget(cancelButton)
+        hbox.addStretch(1)
         # 把这个水平布局放到了一个垂直布局盒里面。弹性元素会把所有的元素一起都放置在应用的右下角。
         vbox = QVBoxLayout()
         vbox.addStretch(1)
         vbox.addLayout(hbox)
+        vbox.addStretch(7)
 
         self.setLayout(vbox)
 
@@ -49,5 +52,5 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = Example()
-    sys.exit(app.exec_()
+    sys.exit(app.exec_())
 
