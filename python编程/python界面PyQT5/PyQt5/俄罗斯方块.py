28698923b65d33e4349a54cda4e884934393ccd4
“yourVincent.Wu <vincent.wu@goertek.com>
2019-04-18 16:46:17 +0800

    pyqt5update

--- python编程/python界面PyQT5/PyQt5/俄罗斯方块.py
+++ python编程/python界面PyQT5/PyQt5/俄罗斯方块.py
@@ -10,7 +10,8 @@ This is a Tetris game clone.
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
-import sys, random
+import sys
+import random
 
 
 class Tetris(QMainWindow):
