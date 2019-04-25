39ed44965ed126a1091b87b37ecf6e59e4ba7064
“yourVincent.Wu <vincent.wu@goertek.com>
2019-04-25 19:33:03 +0800

    hough圆形变换

--- Computer Vision/00OpenCV/27.1分水岭算法图像分割.py
+++ Computer Vision/00OpenCV/27.1分水岭算法图像分割.py
@@ -3,7 +3,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-img = cv2.imread('coins.png')
+img = cv2.imread('image/water_coins.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
@@ -12,6 +12,7 @@ kernel = np.ones((3, 3), np.uint8)
 opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
 # sure background area
 sure_bg = cv2.dilate(opening, kernel, iterations=3)
+cv2.imshow("sure_bg", sure_bg)
 # Finding sure foreground area
 # 距离变换的基本含义是计算一个图像中非零像素点到最近的零像素点的距离，也就是到零像素点的最短距离
 # 个最常见的距离变换算法就是通过连续的腐蚀操作来实现，腐蚀操作的停止条件是所有前景像素都被完全
@@ -23,8 +24,9 @@ dist_transform = cv2.distanceTransform(opening, 1, 5)
 ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255, 0)
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
+cv2.imshow("sure_fg", sure_fg)
 unknown = cv2.subtract(sure_bg, sure_fg)
-
+cv2.imshow("unknown", unknown)
 
 # Marker labelling
 ret, markers1 = cv2.connectedComponents(sure_fg)
@@ -38,6 +40,6 @@ markers[unknown == 255] = 0
 markers3 = cv2.watershed(img, markers)
 img[markers3 == -1] = [255, 0, 0]
 
-
-markers = cv2.watershed(img, markers)
-img[markers == -1] = [255, 0, 0]
+cv2.imshow("marker", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
