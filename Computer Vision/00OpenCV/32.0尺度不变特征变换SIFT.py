52d9bd1afef4f32b20dd764ca9add9a28ada6934
WJay <1016892600@qq.com>
2019-05-03 13:44:51 +0800

    特征提取，关键点检测-角点

---
+++ Computer Vision/00OpenCV/32.0尺度不变特征变换SIFT.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+
+img = cv2.imread('./image/home.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.SIFT()
+kp = sift.detect(gray, None)
+
+img = cv2.drawKeypoints(gray, kp)
+
+cv2.imwrite('sift_keypoints.jpg', img)
+
+# 这个算法包含在 OpenCV 中的收费模块中。
