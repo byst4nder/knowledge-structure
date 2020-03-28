f3f4d736db4c93685e45bafa14dca5cc42c9611f
vincent.wu <1016892600@qq.com>
2020-03-28 23:10:49 +0800

    update

---
+++ temp/配附/00布局UI.py
@@ -0,0 +1,5 @@
+# 搭建界面完成，下面需要对不同尺寸下的窗口大小进行适应。自由变换。
+# 切换电脑之后发现这个问题：
+# Qt Creator 窗体控件自适应窗口大小布局
+# https://www.cnblogs.com/emouse/archive/2013/05/19/3087708.html
+# https://blog.csdn.net/void_xinyue/article/details/94408089
